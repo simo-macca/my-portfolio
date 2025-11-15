b8b1bb3d9991ff9fe0ca71b640cfd326cdabfb41
Simone <simone.maccario04@gmail.com>
2025-11-15 19:08:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="w-full">
+      <nav class="mx-auto flex w-full items-center justify-evenly p-4">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
