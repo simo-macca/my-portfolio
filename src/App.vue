a70971d053e96c00d73ce64aa6859f81591a5310
Simone <simone.maccario04@gmail.com>
2025-11-15 22:09:14 +0100

    restored the base code from where to start

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header> -->
-  <!--   <div class="w-full"> -->
-  <!--     <nav class="mx-auto flex w-full items-center justify-evenly p-4"> -->
-  <!--       <RouterLink to="/">Home</RouterLink> -->
-  <!--       <RouterLink to="/about">About</RouterLink> -->
-  <!--     </nav> -->
-  <!--   </div> -->
-  <!-- </header> -->
+  <header>
+    <div class="w-full">
+      <nav class="mx-auto flex w-full items-center justify-evenly p-4">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
 
   <RouterView />
 </template>
